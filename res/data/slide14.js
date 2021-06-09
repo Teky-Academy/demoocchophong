c3d1eb57353e26f29a22516e39f12ea7f73fc15b
minminhminh <minhnh.dhsp@gmail.com>
2021-06-09 23:27:26 +0700

    demo

---
+++ res/data/slide14.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{A9C04E10-11A6-49B3-AC02-519358B658C8}'];loadHandler&&loadHandler(13, '<div id="spr0_1123b6e"><div id="spr1_1123b6e" class="kern slide"><img id="img0_1123b6e" src="data/img3.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_1123b6e" class="kern slide"></div></div>', '{"s":[]}');})();
